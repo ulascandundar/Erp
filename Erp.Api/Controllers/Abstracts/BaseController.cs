586f9a412d0f4343cc5ad3bfd4780af86b292f4f
ulascan <ulascan@DESKTOP-QUH4MNL>
2025-03-05 11:20:03 +0300

    Add Excel and CSV export functionality to BaseController
    This commit introduces methods for exporting data to Excel and CSV formats in the `BaseController` class. A new attribute, `ExcelColumnNameAttribute`, is added for customizing column names during export. The `ClosedXML` library is included as a dependency to facilitate Excel file creation.
    
    The `ExportToExcel` and `ExportToCsv` methods are implemented to handle data export, and the `EscapeCsvField` method is added to manage special characters in CSV fields. Additionally, the project file (`Erp.Api.csproj`) is updated to include the `ClosedXML` package.

--- Erp.Api/Controllers/Abstracts/BaseController.cs
+++ Erp.Api/Controllers/Abstracts/BaseController.cs
@@ -1,5 +1,10 @@
-﻿using Erp.Domain.Models;
+﻿using ClosedXML.Excel;
+using Erp.Domain.Models;
+using Erp.Infrastructure.Attributes.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Reflection;
+using System.Text;
 
 namespace Erp.Api.Controllers.Abstracts;
 
@@ -29,5 +34,109 @@ public abstract class BaseController : ControllerBase
 		};
 
 		return BadRequest(response);
+	}
+
+	public static FileContentResult ExportToExcel<T>(List<T> data, string fileName)
+	{
+		using (var workbook = new XLWorkbook())
+		{
+			var worksheet = workbook.Worksheets.Add("Sheet1");
+
+			// Verileri bir DataTable'e dönüştür
+			var dataTable = new DataTable();
+			var properties = typeof(T).GetProperties();
+
+			foreach (var property in properties)
+			{
+				// Attribute'dan kolon adını al, yoksa property adını kullan
+				var columnNameAttribute = property.GetCustomAttribute<ExcelColumnNameAttribute>();
+				string columnName = columnNameAttribute != null ? columnNameAttribute.ColumnName : property.Name;
+
+				dataTable.Columns.Add(columnName, Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType);
+			}
+
+			foreach (var item in data)
+			{
+				var row = dataTable.NewRow();
+				foreach (var property in properties)
+				{
+					// Attribute'dan kolon adını al, yoksa property adını kullan
+					var columnNameAttribute = property.GetCustomAttribute<ExcelColumnNameAttribute>();
+					string columnName = columnNameAttribute != null ? columnNameAttribute.ColumnName : property.Name;
+
+					row[columnName] = property.GetValue(item);
+				}
+				dataTable.Rows.Add(row);
+			}
+
+			// DataTable'i Excel sayfasına ekleyin
+			worksheet.Cell(1, 1).InsertTable(dataTable);
+
+			// Excel dosyasını oluşturmak için MemoryStream kullanın
+			using (var stream = new MemoryStream())
+			{
+				workbook.SaveAs(stream);
+				stream.Position = 0;
+
+				var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+				return new FileContentResult(stream.ToArray(), contentType)
+				{
+					FileDownloadName = fileName + ".xlsx"
+				};
+			}
+		}
+	}
+
+	public static FileContentResult ExportToCsv<T>(List<T> data, string fileName)
+	{
+		var properties = typeof(T).GetProperties();
+		var columnNames = new List<string>();
+
+		// Get column names from attributes or property names
+		foreach (var property in properties)
+		{
+			var columnNameAttribute = property.GetCustomAttribute<ExcelColumnNameAttribute>();
+			string columnName = columnNameAttribute != null ? columnNameAttribute.ColumnName : property.Name;
+			columnNames.Add(columnName);
+		}
+
+		// Create CSV content
+		var sb = new StringBuilder();
+
+		// Add header row
+		sb.AppendLine(string.Join(",", columnNames.Select(EscapeCsvField)));
+
+		// Add data rows
+		foreach (var item in data)
+		{
+			var values = new List<string>();
+			foreach (var property in properties)
+			{
+				var value = property.GetValue(item);
+				values.Add(EscapeCsvField(value?.ToString() ?? string.Empty));
+			}
+			sb.AppendLine(string.Join(",", values));
+		}
+
+		// Convert to byte array
+		var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+
+		// Return as file
+		var contentType = "text/csv";
+		return new FileContentResult(bytes, contentType)
+		{
+			FileDownloadName = fileName + ".csv"
+		};
+	}
+
+	private static string EscapeCsvField(string field)
+	{
+		// If the field contains a comma, newline, or double quote, it needs to be escaped
+		if (field.Contains(",") || field.Contains("\"") || field.Contains("\n") || field.Contains("\r"))
+		{
+			// Double up any double quotes and enclose the entire field in double quotes
+			return "\"" + field.Replace("\"", "\"\"") + "\"";
+		}
+		return field;
 	}
 }
---
+++ Erp.Infrastructure/Attributes/Data/ExcelColumnNameAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erp.Infrastructure.Attributes.Data;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+public class ExcelColumnNameAttribute : Attribute
+{
+	public string ColumnName { get; }
+
+	public ExcelColumnNameAttribute(string columnName)
+	{
+		ColumnName = columnName;
+	}
+}
